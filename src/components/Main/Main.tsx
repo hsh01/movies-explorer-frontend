a192cceb46f192388a08d125a0cbeddd6c7eee60
Shamil K <hsh0@ya.ru>
2023-02-25 15:33:13 +0300

    init

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import 'React';
+import React from "react";
+import styles from './Main.module.css';
+
+const Main: React.FC = () => {
+    return (
+        <div className={styles.Main}></div>
+    );
+};
+
+export {Main};
